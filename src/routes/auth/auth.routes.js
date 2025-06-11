d0c3b7b7f66b05baf99277e6f89948feebed098a
frnnd <fdcg.contacto1@gmail.com>
2025-06-10 22:56:45 -0400

    feat: rutas login y register

--- src/routes/auth/auth.routes.js
+++ src/routes/auth/auth.routes.js
@@ -1,1 +1,13 @@
-//Rutas para el proceso auth
+const { Router } = require("express");
+const {
+  login,
+  register,
+} = require("../../controllers/authController");
+
+const router = Router();
+//POST /login → Ingreso de un usuario registrado.
+router.post("/login", login);
+//POST /register → Registrar un usuario.
+router.post("/register", register);
+
+module.exports = router;
