92f99fa6dae69492c1074535474d5fbd388c6afd
frnnd <fdcg.contacto1@gmail.com>
2025-06-10 23:24:13 -0400

    feat: ruta 'me' para obtener la informacion del perfil registrado

--- src/routes/user/user.routes.js
+++ src/routes/user/user.routes.js
@@ -1,1 +1,13 @@
-//Rutas para los usuarios
+const { Router } = require("express");
+const {
+  getUser,
+} = require("../../controllers/userController");
+const jwtValidate = require("../../middlewares/jwtValidate");
+
+
+const router = Router();
+
+//GET /me → ver la información del usuario registrado.
+router.get("/me", jwtValidate, getUser);
+
+module.exports = router;
