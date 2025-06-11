ecb81c6001631a938de32ddfa1eadee50d81c6b5
frnnd <fdcg.contacto1@gmail.com>
2025-06-10 22:25:55 -0400

    feat: agrega el usuario base, quien realiza los prestamos del sistema (administrador del sistema)

--- src/seeders/20250610223503-seed-user.js
+++ src/seeders/20250610223503-seed-user.js
@@ -1,25 +1,23 @@
 'use strict';
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync(10);
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    return await queryInterface.bulkInsert('user', [
+      {
+        name: 'Jose',
+        lastName: 'Benitez',
+        email: 'jose.benitez@ce.ucn.cl',
+        password: bcrypt.hashSync('jbenitez123', salt),
+        roleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+    ]);
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    return await queryInterface.bulkDelete('user', null, {});
   }
 };
