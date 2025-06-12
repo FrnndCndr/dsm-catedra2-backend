ff562141b28a46d26ad779bf479723a0cad0138e
frnnd <fdcg.contacto1@gmail.com>
2025-06-12 09:40:52 -0400

    docs: agrega documentacion a los endpoints de auth y user

--- src/routes/auth/auth.routes.js
+++ src/routes/auth/auth.routes.js
@@ -5,9 +5,14 @@ const {
 } = require("../../controllers/authController");
 
 const router = Router();
+
 //POST /login → Ingreso de un usuario registrado.
+//● Ingresar correo electrónico, contraseña.
 router.post("/login", login);
+
 //POST /register → Registrar un usuario.
+//● Ingresar nombre, apellido, correo electrónico, contraseña.
+//● La contraseña debe ser hasheada antes de ingresar a la base de datos.
 router.post("/register", register);
 
 module.exports = router;
--- src/routes/user/user.routes.js
+++ src/routes/user/user.routes.js
@@ -2,12 +2,13 @@ const { Router } = require("express");
 const {
   getUser,
 } = require("../../controllers/userController");
-const jwtValidate = require("../../middlewares/jwtValidate");
 
+const jwtValidate = require("../../middlewares/jwtValidate");
 
 const router = Router();
 
 //GET /me → ver la información del usuario registrado.
+//● El usuario puede obtener la información de su perfil, exceptuando la contraseña.
 router.get("/me", jwtValidate, getUser);
 
 module.exports = router;
